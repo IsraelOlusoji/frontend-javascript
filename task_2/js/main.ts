3d88658288a5ef42dc26cb88aa7ab4e25159544c
jungles001 <olusojiisraelife@gmail.com>
2025-09-11 22:53:58 +0100

    . Creating functions specific to employees

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -1,6 +1,21 @@
-class Teacher {}
-class Director {}
+class Teacher {
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+class Director {
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
 
 function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks;
+  }
+  return employee.workTeacherTasks;
 }
